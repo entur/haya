d362139495e57a64c171878871600bce1a87ca2b
mansoor.sajjad <sh.mansoor.com@gmail.com>
2022-10-13 20:50:01 +0200

    Removing the spring cloud task.

--- src/main/java/org/entur/haya/HayaApplication.java
+++ src/main/java/org/entur/haya/HayaApplication.java
@@ -1,15 +1,28 @@
 package org.entur.haya;
 
+import org.apache.camel.CamelContext;
+import org.apache.camel.FluentProducerTemplate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.task.configuration.EnableTask;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
-@EnableTask
 public class HayaApplication {
+
+    private final CamelContext camelContext;
+
+    public HayaApplication(CamelContext camelContext) {
+        this.camelContext = camelContext;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(HayaApplication.class, args);
     }
 
+    @EventListener(ApplicationReadyEvent.class)
+    public void doSomethingAfterStartup() {
+        FluentProducerTemplate fluentProducerTemplate = camelContext.createFluentProducerTemplate();
+        fluentProducerTemplate.to("direct:makeCSV").request();
+    }
 }
